1f3aee64b9b5329b1ff75998a498661facb39dac
Finn Rothacker <finnrothacker@Finns-MacBook-Pro.local>
2020-11-09 16:50:18 +1100

    initial commit

---
+++ creds.py
@@ -0,0 +1,27 @@
+
+xxx = {
+    "username": "xxx",
+    "password": "xxx"
+}
+
+xxxx = {
+    "xxx": "xxx",
+    "xxx": "xxx"
+}
+
+xxx = {
+    "username": "xxx",
+    "password": "xxx"
+}
+
+xxx = {
+    "username": "xxx",
+    "password": "xxx"
+}
+
+xxx = {
+    "username": "xxx",
+    "password": "xxx"
+}
+
+accounts = [account, account]
